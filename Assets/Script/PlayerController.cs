31d0b4c8d1cc3613ad5bb5a145d7c12829880b96
mteworldasd <139207526+mteworldasd@users.noreply.github.com>
2023-12-18 18:36:03 +0900

    23 12 18
    game system make

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float jumpForce;
+    public bool onGround = true;
+    public bool gameOver;
+    public Text GameoverText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            onGround = false;
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
+    }
+
+    public void GameOver()
+    {
+        GameoverText.gameObject.SetActive(true);
+    }
+}
